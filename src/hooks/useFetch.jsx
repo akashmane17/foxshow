7c6b1215b3cd5d8630bb663090cad85cf92ebc64
Akash Mane <akashmane277@gmail.com>
2023-09-27 13:47:07 +0530

    uploading to git

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+//this custom hook takes Url as the parameter and return data, loading, and error
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading("loading...");
+        setData(null);
+        setError(null);
+
+
+        fetchDataFromApi(url)
+            .then((res) => {
+                setLoading(false);
+                setData(res);
+            })
+            .catch((err) => {
+                setLoading(false);
+                setError("Something went wrong!");
+            });
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useFetch;
